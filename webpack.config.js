85c7ece1fb57c4f1d63fb90f78133152de0bb576
Jean-Charles Sisk <jasisk@gmail.com>
2015-09-09 00:54:09 +0530

    adding prod build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.join(__dirname, 'server', 'public'),
+    path: path.join(__dirname, 'server', 'public', 'js'),
     publicPath: '/',
     filename: 'bundle.js'
   },
